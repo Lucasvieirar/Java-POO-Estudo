b5d7c031fd387cf76f3e8bff3a88f879d649f7f6
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-03 23:39:54 -0300

    Aula 2 Visibilidade

---
+++ Aula02-Visibilidade de Atributos e Métodos/Aula2.java
@@ -0,0 +1,11 @@
+package aula2;
+public class Aula2 {
+    public static  void main(String[] args){
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bic";
+        c1.cor = "azul";
+        c1.carga = 80;
+        c1.tampada = true;
+        c1.status();
+    }
+}
---
+++ Aula02-Visibilidade de Atributos e Métodos/Caneta.java
@@ -0,0 +1,31 @@
+package aula2;
+public class Caneta{
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected tampada;
+    void status(){
+        System.out.println("Uma caneta " + this.cor );
+        System.out.println("Está tampada? " + this.tampada );
+        System.out.println("Ponta" + this.ponta);
+        System.out.println("Carga" + this.carga);
+        System.out.println("modelo" + this.modelo);
+    }
+   public void rabiscar(){
+        if(this.tampada == true){
+            System.out.println("Caneta tampada");
+        } else{
+            System.out.println("Rabiscando")
+        }
+
+    }
+
+   public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+}
