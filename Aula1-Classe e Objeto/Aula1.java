ded7e751eba75fe350388ccb3985c0da18219f2e
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-03 23:21:19 -0300

    Aula01- Classe e Objeto

---
+++ Aula1-Classe e Objeto/Aula1.java
@@ -0,0 +1,20 @@
+package aula1;
+public class Aula1 {
+    public static  void main(String[] args){
+            Caneta c1 = new Caneta();
+            c1.modelo  = "Bic"
+            c1.cor = "Azul";
+            c1.ponta = 0.5f;
+            c1.tampar();
+            c1.rabidcar();
+            c1.status();
+
+            Caneta c2 = new Caneta();
+            c1.modelo  = "Pilot"
+            c1.cor = "Preta";
+            c1.ponta = 0.8f;
+            c1.destampar();
+            c1.rabidcar();
+            c1.status();
+    }
+}
---
+++ Aula1-Classe e Objeto/Caneta.java
@@ -0,0 +1,31 @@
+package aula1;
+public class Caneta{
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status(){
+        System.out.println("Uma caneta " + this.cor );
+        System.out.println("Está tampada? " + this.tampada );
+        System.out.println("Ponta" + this.ponta);
+        System.out.println("Carga" + this.carga);
+        System.out.println("modelo" + this.modelo);
+    }
+    void rabiscar(){
+        if(this.tampada == true){
+            System.out.println("Caneta tampada");
+        } else{
+            System.out.println("Rabiscando")
+        }
+
+    }
+
+    void tampar(){
+        this.tampada = true;
+    }
+
+    void destampar() {
+        this.tampada = false;
+    }
+}
