e23a2b343f03aba669b42ba377d1941e69af9e5c
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-10 15:03:34 -0300

    vrigula

--- Aula1-Classe e Objeto/Aula1.java
+++ Aula1-Classe e Objeto/Aula1.java
@@ -2,7 +2,7 @@ package aula1;
 public class Aula1 {
     public static  void main(String[] args){
             Caneta c1 = new Caneta();
-            c1.modelo  = "Bic"
+            c1.modelo  = "Bic";
             c1.cor = "Azul";
             c1.ponta = 0.5f;
             c1.tampar();
@@ -10,7 +10,7 @@ public class Aula1 {
             c1.status();
 
             Caneta c2 = new Caneta();
-            c1.modelo  = "Pilot"
+            c1.modelo  = "Pilot";
             c1.cor = "Preta";
             c1.ponta = 0.8f;
             c1.destampar();
